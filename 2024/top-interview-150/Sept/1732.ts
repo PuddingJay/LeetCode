e43989bde6ea5506702038418ce0be47ccc7a526
Windu <windu.intimap@gmail.com>
2024-09-27 15:14:12 +0700

    Solve: Find the Highest Altitude

---
+++ 2024/top-interview-150/Sept/1732.ts
@@ -0,0 +1,11 @@
+const largestAltitude = (gain: number[]): number => {
+  let currentAltitude = 0
+  let highestAltitude = 0
+
+  for(let i = 0; i < gain.length; i++){
+    currentAltitude += gain[i]
+    highestAltitude = Math.max(highestAltitude, currentAltitude) 
+  }
+
+  return highestAltitude
+}
