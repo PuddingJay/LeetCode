96042d26f3b4dbf51cd5c7bce8c161c885c2ac7f
PuddingJay <muhammadwindu27@gmail.com>
2024-12-05 15:58:37 +0700

    solve Duplicate II

---
+++ 2024/Dec/219.ts
@@ -0,0 +1,9 @@
+    const containsNearbyDuplicate = (nums: number[], k: number): boolean => {
+    const map = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i]) && i - map.get(nums[i])! <= k) return true;
+        map.set(nums[i], i);
+    }
+    return false;
+    }
