35412b849a500817d97b0b4ba38fbf06654840af
PuddingJay <muhammadwindu27@gmail.com>
2025-01-30 16:05:15 +0700

    solve create hello world function

---
+++ 2025/2667.ts
@@ -0,0 +1,11 @@
+function createHelloWorld() {
+    
+    return function(...args:any[]): string {
+        return 'Hello World';
+    };
+};
+
+const helloWorld = createHelloWorld();
+
+console.log(helloWorld()); // Hello World!
+
